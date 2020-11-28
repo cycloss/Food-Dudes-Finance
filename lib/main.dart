24af6e096596154650dd83f7ae8806d07e7ff7a1
Ccyclos <lucasroweeh@gmail.com>
2020-11-28 14:10:47 +0000

    added test file

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed: _incrementCounter,
               color: Colors.blue,
             ),
+            Text('Hello Alex'),
             Text(
               'You have pushed the button this many times:',
             ),
